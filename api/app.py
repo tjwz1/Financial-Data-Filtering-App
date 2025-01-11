32ea4be352ddcfaea0ba1266a747e13dc08dead4
Thomas Zhao <Thomasjzhao@gmail.com>
2025-01-11 18:29:14 -0500

    q

--- api/app.py
+++ api/app.py
@@ -79,5 +79,5 @@ def get_data():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#if __name__ == '__main__':
+    #app.run(debug=True)
